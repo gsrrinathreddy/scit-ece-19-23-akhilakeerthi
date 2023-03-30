eacf22cf1cd5528a945e23c39d8f46e12afa79b8
Akhilakeerthi55 <128037117+Akhilakeerthi55@users.noreply.github.com>
2023-03-30 22:07:12 +0530

    project1

---
+++ project1/src/Pages/About/index.js
@@ -0,0 +1,14 @@
+import { Typography } from "@mui/material"
+
+export default function Aboutus(){
+    return(
+        <>
+<Typography>Name: Akhila Keerthi</Typography>
+<Typography>Father Name: Sammaiah</Typography>
+<Typography>Mother Name: Sunitha</Typography>
+<Typography>Email: akhilakeerthi3218@gmail.com</Typography>
+<Typography>Phone No.: 8328165740</Typography>
+        </>
+    
+    )
+    }
