f9894d2531b59d18b7ab901e6c0d3c83d6d503c7
Akhilakeerthi55 <128037117+Akhilakeerthi55@users.noreply.github.com>
2023-04-02 23:45:55 +0530

    project1 adding

--- project1/src/App.js
+++ project1/src/App.js
@@ -7,6 +7,9 @@ import Experience from './Pages/Experience';
 import Hobbies from './Pages/Hobbies';
 import Qualifications from './Pages/Qualifications';
 import Skills from './Pages/Skills';
+import Projects from './Pages/Projects';
+import Certifications from './Pages/Certifications';
+import Workshop from './Pages/Workshop';
 
 function App() {
   return(
@@ -19,6 +22,9 @@ function App() {
     <Route path ="Hobbies" element={<Hobbies/>}/>
     <Route path ="Qualifications" element={<Qualifications/>}/>
     <Route path ="Skills" element={<Skills/>}/>
+    <Route path ="Projects" element={<Projects/>}/>
+    <Route path ="Certifications" element={<Certifications/>}/>
+    <Route path ="Workshop" element={<Workshop/>}/>
     </Routes>
     </BrowserRouter>
     </div>
--- project1/src/Components/Navbar/index.js
+++ project1/src/Components/Navbar/index.js
@@ -14,7 +14,7 @@ import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 import { Link } from 'react-router-dom';
 
-const pages = ['About', 'Experience', 'Hobbies','Qualifications','Skills'];
+const pages = ['About', 'Experience', 'Hobbies','Qualifications','Skills','Projects','Certifications','Workshop'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 function Navbar() {
